08fa81efdaa926fa6b4b0637c7ed692c06e88640
Frank <franckmy@outlook.com>
2015-12-08 10:47:44 +0800

    v1

--- showmithecode/0010/1.py
+++ showmithecode/0010/1.py
@@ -5,7 +5,7 @@ import random,string
 
 IMAGE_MODE = 'RGB'
 IMAGE_BG_COLOR = (255,255,255)
-Image_Font = 'arial.tff'
+Image_Font = 'E:/python/Users/Frank/PycharmProjects/python/python/showmithecode/0010/arial.ttf'
 
 a= string.letters
 text = ''.join(random.choice(a) for i in range(4))
@@ -17,6 +17,17 @@ def colorRandom():
 width,height,chance= 400,200,2
 
 im = Image.new(IMAGE_MODE,(width,height),IMAGE_BG_COLOR)
-
-
-
+draw = ImageDraw.Draw(im)
+for w in xrange(width):
+    for h in xrange(height):
+        if chance < random.randint(1,100):
+            draw.point((w,h),fill=colorRandom())
+font =ImageFont.truetype(Image_Font,80)
+font_width,font_height = font.getsize(text)
+strs_len = len(text)
+x = (width - font_width)/4
+y = (height - font_height)*2/5
+for i in text:
+    draw.text((x,y),i,colorRandom(),font)
+    x+=font_width/strs_len+(width-font_width)/6
+im.show()
--- showmithecode/signinn/signin.py
+++ showmithecode/signinn/signin.py
@@ -1,1 +1,9 @@
+#coding=utf8
 __author__ = 'Frank'
+import time
+a = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+b = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()+60*60*9))
+f = open('C:\Users\Frank\Desktop\signin.txt','w')
+f.write(a+'\n')
+f.write(b+'\n')
+f.close()
